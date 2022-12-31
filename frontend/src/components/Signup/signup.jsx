717a4070b1fa1e6ed8493e1535f3a44a9b2f2c19
Chinmoy <chinmoy.dehingia@gmail.com>
2023-01-01 01:32:19 +0530

    signup page linked with the backend

--- frontend/src/components/Signup/signup.jsx
+++ frontend/src/components/Signup/signup.jsx
@@ -1,7 +1,20 @@
 import { Link } from 'react-router-dom'
 import "./signupBig.css"
 import './signup.css'
+import axios from 'axios'
+import { useState } from 'react'
+import {useNavigate} from "react-router-dom"
 const Signup = ()=>{
+    const navigate = useNavigate();
+    const [loading,setLoading]=useState(false);
+    const [data,setData]=useState({email:"",fullname:"",username:"",password:""})
+    const handleSignup=async (e)=>{
+        e.preventDefault()
+        setLoading(true)
+        const user = await axios.post("http://localhost:3004/signup",data);
+        setLoading(false);
+        navigate("/");
+    }
     return(
         <>
         <div className="signup-container">
@@ -12,11 +25,12 @@ const Signup = ()=>{
             </div>
             <div className="signup-bottom">
                 <form action="">
-                    <input type="email" placeholder='Email' required/>
-                    <input type="text" placeholder='Full Name' required/>
-                    <input type="text" placeholder='Username' required/>
-                    <input type="password" placeholder='Password' required/>
-                    <button id="signup" type='submit'>Signup</button>
+                    <input type="email" placeholder='Email' required onChange={(e)=>{setData({...data,email:e.target.value})}}/>
+                    <input type="text" placeholder='Full Name' required onChange={(e)=>{setData({...data,fullname:e.target.value})}}/>
+                    <input type="text" placeholder='Username' required onChange={(e)=>{setData({...data,username:e.target.value})}}/>
+                    <input type="password" placeholder='Password' required onChange={(e)=>{setData({...data,password:e.target.value})}}/>
+                    <button id="signup" type='submit' onClick={handleSignup}>Signup</button>
+                    {loading&&<div>Registering ......</div>}
                 </form>
             </div>
         </div>
