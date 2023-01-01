3b805a3e1ab968e9355cad1931b5cb23378c56d1
Chinmoy <chinmoy.dehingia@gmail.com>
2023-01-01 10:59:51 +0530

    Login Route created

--- backend/Routes/loginAndSignup.js
+++ backend/Routes/loginAndSignup.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const signup = require("../Models/signup")
 const bcrypt = require("bcrypt");
-const e = require("express");
+const jwt = require("jsonwebtoken")
 
 //Signup Route
 router.post("/signup",async (req,res)=>{
@@ -40,5 +40,37 @@ router.post("/signup",async (req,res)=>{
     }
 })
 
-
+//login route
+router.post("/login",async (req,res)=>{
+    try{
+        const {email,password}=req.body;
+        const user = await signup.find({email:req.body.email});
+        if(user.length==0){
+            return res.status(404).json({
+                status:"failed",
+                message:"No user Found"
+            })
+        }
+        bcrypt.compare(password,user[0].password,(err,result)=>{
+            if(result){
+                const token = jwt.sign({user_id:user[0]._id},process.env.MY_JWT)
+                return res.status(200).json({
+                    status:"success",
+                    message:"Logged in",    
+                    token
+                })
+            }else{
+                res.status(404).json({
+                    status:"failed",
+                    message:"password didn't match"
+                })
+            }
+        })
+    }catch(e){
+        res.status(400).json({
+            status:"failed",
+            message:e.message
+        })
+    }
+})
 module.exports = router;
